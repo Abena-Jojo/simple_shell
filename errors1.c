3903725b8cb5abbe7e93f33b5fd6efbb4815c287
jobren95 <jobren95@gmail.com>
2023-06-27 08:36:55 -0700

    errors.c

--- errors1.c
+++ errors1.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- * _erratoi - converts a string to an integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
+ * _erratoi - function to converts an array to an integer
+ * @s: the array to be converted
+ * Return: 0 if no counts in array, converted counts otherwise
  *       -1 on error
  */
 int _erratoi(char *s)
@@ -29,10 +29,10 @@ int _erratoi(char *s)
 }
 
 /**
- * print_error - prints an error message
+ * print_error - function that prints error message
  * @info: the parameter & return info struct
- * @estr: string containing specified error type
- * Return: 0 if no numbers in string, converted number otherwise
+ * @estr: array containing specified error type
+ * Return: 0 if no counts in array, converted counts otherwise
  *        -1 on error
  */
 void print_error(info_t *info, char *estr)
@@ -47,11 +47,11 @@ void print_error(info_t *info, char *estr)
 }
 
 /**
- * print_d - function prints a decimal (integer) number (base 10)
- * @input: the input
- * @fd: the filedescriptor to write to
+ * print_d - function prints a decimal (integer) counts (base 10)
+ * @input: the value
+ * @fd: write to the filedescriptor
  *
- * Return: number of characters printed
+ * Return: counts of characters printed
  */
 int print_d(int input, int fd)
 {
@@ -87,11 +87,11 @@ int print_d(int input, int fd)
 
 /**
  * convert_number - converter function, a clone of itoa
- * @num: number
+ * @num: counts
  * @base: base
  * @flags: argument flags
  *
- * Return: string
+ * Return: array
  */
 char *convert_number(long int num, int base, int flags)
 {
@@ -123,7 +123,7 @@ char *convert_number(long int num, int base, int flags)
 
 /**
  * remove_comments - function replaces first instance of '#' with '\0'
- * @buf: address of the string to modify
+ * @buf: the array of address to modify
  *
  * Return: Always 0;
  */
