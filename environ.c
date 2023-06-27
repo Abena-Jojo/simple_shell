c5646aa8e00fb2969b937e2063eb1523f5091ce5
jobren95 <jobren95@gmail.com>
2023-06-27 07:50:04 -0700

    environ.c

--- environ.c
+++ environ.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
+ * _myenv - function that prints current environment
+ * @info: potential arguments containning structures to maintain
  *          constant function prototype.
  * Return: Always 0
  */
@@ -13,8 +13,8 @@ int _myenv(info_t *info)
 }
 
 /**
- * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
+ * _getenv - function that gets value of an environ variable
+ * @info: potential arguments containning structures to maintain
  * @name: env var name
  *
  * Return: the value
@@ -35,9 +35,9 @@ char *_getenv(info_t *info, const char *name)
 }
 
 /**
- * _mysetenv - Initialize a new environment variable,
+ * _mysetenv - declare a new environment variable,
  *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
+ * @info: potential arguments containning structure to maintain
  *        constant function prototype.
  *  Return: Always 0
  */
@@ -55,7 +55,7 @@ int _mysetenv(info_t *info)
 
 /**
  * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
+ * @info: potential arguments containing structure to maintain
  *        constant function prototype.
  * Return: Always 0
  */
@@ -76,7 +76,7 @@ int _myunsetenv(info_t *info)
 
 /**
  * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
+ * @info: potential arguments containing structures to maintain
  *          constant function prototype.
  * Return: Always 0
  */
