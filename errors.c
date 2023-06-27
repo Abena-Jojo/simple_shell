577556e703514ed695279ce527ccbde600b915c0
jobren95 <jobren95@gmail.com>
2023-06-27 08:02:19 -0700

    errors.c

--- errors.c
+++ errors.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * _eputs - prints an input string
- * @str: the string to be printed
+ * _eputs - function that prints an input array
+ * @str: the array to be printed
  *
  * Return: Nothing
  */
@@ -20,7 +20,7 @@ void _eputs(char *str)
 }
 
 /**
- * _eputchar - writes the character c to stderr
+ * _eputchar - function to write the character c to stderr
  * @c: The character to print
  *
  * Return: On success 1.
@@ -42,9 +42,9 @@ int _eputchar(char c)
 }
 
 /**
- * _putfd - writes the character c to given fd
+ * _putfd - function to writes the character c to given fd
  * @c: The character to print
- * @fd: The filedescriptor to write to
+ * @fd: write to the filedescriptor
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -65,9 +65,9 @@ int _putfd(char c, int fd)
 }
 
 /**
- * _putsfd - prints an input string
- * @str: the string to be printed
- * @fd: the filedescriptor to write to
+ * _putsfd - function that prints an input array
+ * @str: the array to be printed
+ * @fd: write to the filedescriptor
  *
  * Return: the number of chars put
  */
